caf2d8d006eaa432f07eb903559bcb4453d818d3
KaiMJ <mjkim2964@gmail.com>
2025-01-06 12:12:26 -0800

    format colmap_data and gaussian_model

---
+++ utils/colmap_data.py
@@ -0,0 +1,69 @@
+import os
+from utils.colmap_utils import read_cameras_binary, read_images_binary, read_points3D_binary
+from utils.matrix import quaternion_to_rotation_vectorized
+import numpy as np
+
+class COLMAP_Data:
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+
+        self.cameras_file = os.path.join(self.root_dir, "cameras.bin")
+        self.images_file = os.path.join(self.root_dir, "images.bin")
+        self.points3D_file = os.path.join(self.root_dir, "points3D.bin")
+
+        self.cameras_data = read_cameras_binary(self.cameras_file)
+        self.images_data = read_images_binary(self.images_file)
+        self.points3D_data = read_points3D_binary(self.points3D_file)
+
+        self.points, self.colors = self.get_points_colors()
+
+        self.frame_names = [v['name'] for v in self.images_data.values()]
+
+        self.width, self.height = self.cameras_data[1]["width"], self.cameras_data[1]["height"]
+
+        self.K, self.distortion = self.get_intrinsics() # K: (3, 3), distortion: (4,)
+        self.E = self.get_extrinsics() # {frame_name: E}
+
+    def get_intrinsics(self):
+        """
+        instrincis from COLMAP: [fx, fy, cx, cy, s, k1, k2, k3]
+
+        K = [fx 0 cx
+            0 fy cy
+            0 0 1]
+        
+        distortion = [s, k1, k2, k3]
+        """
+        intrinsics = np.array(self.cameras_data[1]["params"])
+        K = np.array([[intrinsics[0], 0, intrinsics[2]],
+                    [0, intrinsics[1], intrinsics[3]],
+                    [0, 0, 1]])
+        distortion = intrinsics[4:]
+        return K, distortion
+    
+    def get_extrinsics(self):
+        """
+        E = [R t
+            0 1]
+
+        returns {frame_name: E}
+        """
+        qvecs = np.array([v['qvec'] for v in self.images_data.values()])
+        tvecs = np.array([v['tvec'] for v in self.images_data.values()])
+
+        rotation = quaternion_to_rotation_vectorized(qvecs)
+        extrinsics = np.zeros((len(self.images_data), 4, 4))
+        extrinsics[:, :3, :3] = rotation
+        extrinsics[:, :3, 3] = tvecs
+        extrinsics[:, 3, 3] = 1
+
+        E = {k: extrinsics[i] for i, k in enumerate(self.frame_names)}
+        return E
+
+    def get_points_colors(self):
+        """
+            Not noticing significant difference between sparse / dense (from original gaussian-splatting repository)
+        """
+        points = np.array([v["xyz"] for v in self.points3D_data.values()])
+        colors = np.array([v["rgb"] / 255.0 for v in self.points3D_data.values()])
+        return points, colors
--- utils/matrix.py
+++ utils/matrix.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# -------- QUATERNION TO ROTATION --------
+
 def quaternion_to_rotation_vectorized(q):
     """
     Convert batch of quaternions to rotation matrices
@@ -27,3 +29,46 @@ def quaternion_to_rotation_vectorized(q):
     r[..., 2, 2] = 1 - 2*(q_i**2 + q_j**2)
     
     return r
+
+# -------- PROJECT POINTS --------
+
+def project_points(points, e, K, dist_coeffs=None):
+    # homogenous coordinates
+    points_h = np.hstack([points, np.ones((len(points), 1))])
+    # First apply extrinsics (4x4 @ 4xN)
+    transformed_points = (e @ points_h.T)
+    # Then get normalized coordinates (before K)
+    transformed_points = transformed_points[:3, :]
+    normalized_coords = transformed_points / transformed_points[2:3, :]  # Divide by Z
+    x, y = normalized_coords[0], normalized_coords[1]
+
+    if dist_coeffs is not None:
+        # Apply distortion
+        r2 = x*x + y*y
+        r4 = r2*r2
+        r6 = r4*r2
+
+        # Radial distortion
+        k1, k2, k3 = dist_coeffs[:3]
+        radial = (1 + k1*r2 + k2*r4 + k3*r6)
+        
+        xd = x * radial
+        yd = y * radial
+
+        # Convert back to homogeneous coordinates
+        normalized_coords = np.vstack((xd, yd, np.ones_like(x)))
+
+    # Now project with intrinsics
+    projected_points = (K @ normalized_coords).T
+    # Already in image coordinates since we applied distortion to normalized coordinates
+    projected_points = projected_points[:, :2]
+
+    return projected_points
+
+def select_points_within_bounds(projected_points, width, height, colors=None):
+    bound_indices = np.all(projected_points >= 0, axis=1) & np.all(projected_points < [width, height], axis=1)
+    bound_points = projected_points[bound_indices]
+    if colors is not None:
+        bound_colors = colors[bound_indices]
+        return bound_points, bound_colors
+    return bound_points, None
